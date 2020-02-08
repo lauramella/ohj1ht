e8d6d0438f370de2cfef263567b336a0f4fd9de1
melllaem <laura.e.mella@student.jyu.fi>
2020-02-09 00:41:06 +0200

    Lisäsin kuvan

---
+++ Konna/Konna.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author laura
+/// @version ..2020
+/// <summary>
+/// 
+/// </summary>
+public class Konna
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+
+    }
+
+}
