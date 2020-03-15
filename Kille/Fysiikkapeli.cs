36fe10d4e6f21dbcbc53b8ae3ebf772de70c0637
melllaem <laura.e.mella@student.jyu.fi>
2020-03-15 20:59:53 +0200

    Muokkasin kenttää.

--- Kille/Fysiikkapeli.cs
+++ Kille/Fysiikkapeli.cs
@@ -20,13 +20,13 @@ public class Kille : PhysicsGame
         SetWindowSize(1024, 768);
         TileMap kentta = TileMap.FromLevelAsset("kentta2");
         kentta.SetTileMethod('x', LuoLattia);
-        kentta.SetTileMethod('1', LuoTaso);
-        kentta.SetTileMethod('2', LuoTaso);
-        kentta.SetTileMethod('3', LuoTaso);
+        kentta.SetTileMethod('t', LuoTaso);
+        kentta.SetTileMethod('T', LuoTaso);
+        kentta.SetTileMethod('a', LuoTaso);
         kentta.SetTileMethod('p', LuoPelaaja);
-        kentta.SetTileMethod('v', LuoVihu, 2);
-        kentta.SetTileMethod('v', LuoVihu, 2);
-        kentta.SetTileMethod('v', LuoVihu, 2);
+        kentta.SetTileMethod('3', LuoVihu, 2, 3);
+        kentta.SetTileMethod('4', LuoVihu, 2, 3);
+        kentta.SetTileMethod('v', LuoVihu, 2, 3);
         kentta.Execute(BLOKIN_LEVEYS, BLOKIN_KORKEUS);
 
         Level.Background.CreateStars();
@@ -44,16 +44,17 @@ public class Kille : PhysicsGame
         pelaaja.Position = paikka;
         pelaaja.Image = LoadImage("kille1");
         Add(pelaaja);
-
+        //AddCollisionHandler(pelaaja, "vihunAmmus", CollisionHandler.DestroyObject);
         Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikuta pelaajaa oikealle", pelaaja, LIIKUTUSVOIMA);
         Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikuta pelaajaa vasemmalle", pelaaja, -LIIKUTUSVOIMA);
         Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Hyppää", pelaaja, HYPPYVOIMA);
-
+        Keyboard.Listen(Key.Space, ButtonState.Pressed, Heita, "Heita ammus", pelaaja, "pelaajanAmmus");
     }
 
-    public void LuoVihu(Vector paikka, double leveys, double korkeus, int liikemaara)
+    public void LuoVihu(Vector paikka, double leveys, double korkeus, int liikemaara, int hp)
     {
-        PhysicsObject vihu = new PhysicsObject(leveys, korkeus);
+     
+        Vihu vihu = new Vihu(leveys, korkeus, hp);
         vihu.Position = paikka;
         vihu.Image = LoadImage("pallo1");
         vihu.Shape = Shape.Circle;
@@ -68,15 +69,40 @@ public class Kille : PhysicsGame
         pfb.Path = reitti;
         pfb.Loop = true;
         vihu.Brain = pfb;
+
+        Timer heittoajastin = new Timer();
+        heittoajastin.Interval = 3.0;
+        heittoajastin.Timeout += delegate () { Heita(vihu, "vihunAmmus"); };
+        heittoajastin.Start();
+        AddCollisionHandler(vihu, "pelaajanAmmus", CollisionHandler.DestroyTarget);
+            AddCollisionHandler(vihu, "pelaajanAmmus", CollisionHandler.AddMeterValue(vihu.HP, -1));
+            vihu.Destroyed += delegate () { heittoajastin.Stop(); };
     }
+
+
+ 
+
+
+
+
+    public void Heita(PhysicsObject heittavaOlio, string tagi)
+    {
+        PhysicsObject heitettava = new PhysicsObject(BLOKIN_LEVEYS / 2, BLOKIN_KORKEUS / 2, Shape.Heart);
+        heitettava.Position = heittavaOlio.Position + new Vector(BLOKIN_LEVEYS/2, BLOKIN_KORKEUS/2);
+        heitettava.Hit(new Vector(800, 200));
+        heitettava.Tag = "vihunAmmus";
+        heitettava.MaximumLifetime = TimeSpan.FromSeconds(3.0);
+        Add(heitettava, 1);
+    }
+
 
     public void Hyppaa(PlatformCharacter hahmo, double voima)
     {
         hahmo.Jump(voima);
     }
-    public void Liikuta(PlatformCharacter liikuteltavaOlio, double suunta)
+    public void Liikuta(PlatformCharacter vihu, double suunta)
     {
-        liikuteltavaOlio.Walk(suunta);
+        vihu.Walk(suunta);
     }
 
     public void LuoTaso(Vector paikka, double leveys, double korkeus)
---
+++ Kille/Vihu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Jypeli;
+
+
+
+public class Vihu : PhysicsObject
+{
+    public IntMeter HP;
+
+    public Vihu(double leveys, double korkeus, int elamapisteet)
+        :base(leveys, korkeus)
+    {
+        HP = new IntMeter(elamapisteet, 0, elamapisteet);
+        HP.LowerLimit += delegate () { this.Destroy(); };
+    }
+
+}
+
